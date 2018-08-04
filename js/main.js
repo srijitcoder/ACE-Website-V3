bdd9106b76b82316e106b018398801427749288b
srijitcoder <srijitsm123@gmail.com>
2018-08-05 05:24:43 +0530

    Some Componenets

---
+++ js/main.js
@@ -0,0 +1,99 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.heading h1', {
+    duration: 1000,
+    scale: 1,
+    opacity: 0,
+    origin: 'bottom'
+});
+
+sr.reveal('.sub-heading p', {
+    duration: 1000,
+    scale: 1,
+    opacity: 0,
+    origin: 'bottom'
+});
+
+sr.reveal('section .content', {
+    duration: 1000,
+    scale: 1,
+    opacity: 0,
+    origin: 'left'
+});
+
+sr.reveal('section .stat-wrap', {
+    duration: 1000,
+    scale: 1,
+    opacity: 0,
+    origin: 'bottom'
+});
+
+sr.reveal('section .team-wrap', {
+    duration: 1000,
+    scale: 1,
+    opacity: 0,
+    origin: 'bottom'
+});
+
+sr.reveal('button', {
+    duration: 500,
+    scale: 0.8,
+    opacity: 0.5
+});
+
+sr.reveal('section .life-content h1', {
+    duration: 1000,
+    scale: 1,
+    opacity: 0,
+    origin: 'top'
+});
+
+sr.reveal('section .life-content p', {
+    duration: 1000,
+    scale: 1,
+    delay:1,
+    opacity: 0,
+    origin: 'top'
+});
+
+sr.reveal('section .life-content .social a', {
+    duration: 1500,
+    scale: 1,
+    opacity: 0,
+    origin: 'bottom'
+});
+
+sr.reveal('section .life-img div', {
+    duration: 1000,
+    scale: 1,
+    opacity: 0,
+    origin: 'right'
+});
+
+sr.reveal('section .wrap-main .img-detail', {
+    duration: 1000,
+    scale: 1,
+    opacity: 0,
+    origin: 'top'
+});
+
+sr.reveal('section .wrap-main .wrap-divide  .wrap-content', {
+    duration: 1000,
+    scale: 1,
+    opacity: 0,
+    origin: 'right'
+});
+
+sr.reveal('section .wrap-hor', {
+    duration: 1000,
+    scale: 1,
+    opacity: 0,
+    origin: 'top'
+});
+
+
+
+
+
+
+
